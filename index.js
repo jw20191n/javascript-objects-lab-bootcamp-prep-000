b132f04c5604123b799cb34151478ee8a991fc5d
Jiayi Wang <clairejw1015@gmail.com>
2019-04-23 23:36:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return object.assign(object,{[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value
+  return object
+}
